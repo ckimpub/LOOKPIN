169cac5cf781983389dc9652a829ed91063493c1
Mk24CT <96875410+Mk24CT@users.noreply.github.com>
2022-02-09 21:39:33 +0900

    [HEADER&MODAL&NAV] - 0.0.1 - 헤더, 모달, 네비게이션 기능 구현

---
+++ function.js
@@ -0,0 +1,11 @@
+function man(){
+    const sel = document.getElementsByClassName('modal')[0];
+
+    sel.style.display="none";
+}
+
+function woman(){
+    const sel = document.getElementsByClassName('modal')[0];
+
+    sel.style.display="none";
+}
